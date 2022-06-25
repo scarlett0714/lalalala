b57b022c04449b78869fa45d648b03012d62f2fc
scarlett0714 <tpttyakfn03@naver.com>
2022-06-26 00:34:32 +0900

    profile

--- app/src/main/java/com/example/airbnb/SearchFragment.kt
+++ app/src/main/java/com/example/airbnb/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.example.airbnb
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.airbnb.databinding.FragmentSearchBinding
 import com.google.android.material.tabs.TabLayoutMediator
+
 
 class SearchFragment : Fragment() {
     lateinit var binding: FragmentSearchBinding
@@ -28,6 +30,7 @@ class SearchFragment : Fragment() {
         TabLayoutMediator(binding.menuContentTb, binding.menuContentVp){
                 tab, position ->
             tab.text = information[position]
+
         }.attach() //tablayout과 viewpager붙이기 : attach
         //TabLayoutMediator : tablayout-viewpager2와 연결해주는 중재자
         //tablayout이 선택될 때 해당 viewpager의 위치를 동기화, viewpager 스크롤 시 tablayout 스크롤 위치 동기화
